e4a791b72ecde2499be29a7aa27e3405fac59b48
RyleFuentes <98087356+RyleFuentes@users.noreply.github.com>
2022-05-13 19:50:19 +0800

    Add files via upload

---
+++ exercise 15/exercise15.js
@@ -0,0 +1,12 @@
+let name = "Name";
+let age = "age";
+
+
+let number = "number";
+let email = "email";
+let mail = "mail";
+
+let program = "program";
+let IDNumber = "ID";
+let year = "year";
+let section = "section";
